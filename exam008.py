2c75b0cdca6a782c9d8966385679a0aa805cd241
baennigans <bgh2584@naver.com>
2023-08-22 17:04:55 +0900

    python 반복문

---
+++ exam008.py
@@ -0,0 +1,36 @@
+'''
+    *
+    **
+    ***
+    ****
+    *****
+'''
+
+for i in range(5):
+    for j in range(i+1):
+        print('*', end='')
+    print()
+
+
+for i in range(5):
+    print('*' * (i+1))
+
+
+for i in range(9):
+    if i < 5:
+        print('*' * (i+1))
+    else:
+        print('*' * (9-i))
+
+
+for i in range(9):
+    cnt = (i+1) if i < 5 else (9-i)
+    print('*' * cnt)
+
+
+
+for i in range(5):
+    if i == 0:
+        print('*' * 5)
+    else:
+        print(' ' * (i-1),'*' * (5-i))
