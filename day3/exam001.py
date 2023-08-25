99cb312114d49f0f10b70ff27f4918d177815ebf
baennigans <bgh2584@gmail.com>
2023-08-25 12:37:29 +0900

    파이썬 기초문법7

--- day3/exam001.py
+++ day3/exam001.py
@@ -1,29 +1,39 @@
-str = "Hello World"
+str = "Hello, World"
 print(str, str.upper(), str.lower())
 print()
 
-strList = str.split()
+strList = str.split(',')
 print(strList)
 print()
 
 str2 = '/'.join(strList)
 print(str2)
 print(f'[{strList[1].lstrip()}]')
+print()
 
 str = '    Hello World     '
 print(f'str : [{str}]')
-print(f'lstrip() : [{str.lstrip()}]')
+print(f'lstrip() : [{str.lstrip(" !")}]')
 print(f'rstrip() : [{str.rstrip()}]')
 print(f'strip() : [{str.strip("! ")}]')
+print()
 
 str = 'hello'
 print(f'str : [{str}]')
 print(f'str : [{str.center(11)}]')
-
+print(f'str : [{str.center(10)}]')
+print(f'str : [{str.ljust(10)}]')
+print(f'str : [{str.ljust(10)}]')
+print(f'str : [{str.rjust(10)}]')
+print(f'str : [{str.zfill(10)}]')
+print()
 
 str = 'Hello World'
-print(f'"o"위치 : {str.index("o")}')
-print(f'"o"위치 : {str.index("o",6)}')
-print(f'"o"위치 : {str.find("o",6)}')
-
-
+print(f'"o" 위치 : {str.index("o")}')
+print(f'"o" 위치 : {str.index("o",6)}')
+print(f'"o" 위치 : {str.find("o",6)}')
+print(f'"o" 위치 : {str.index("o")}') # 문자열이 없는 경우 예외발생
+print(f'"o" 위치 : {str.find("o")}')  # 문자열이 없는 경우 -1 반환
+print(f'"o" 위치 : {str.rfind("o")}')
+print(f'"l" 개수 : {str.count("l")}')
+print(f'"l" => "rr" 변환 : {str.replace("l", "rr")}')
--- day3/exam003.py
+++ day3/exam003.py
@@ -1,21 +1,34 @@
+'''
+    setdefult   새데이터삽입
+    update      key 없으면 데이터삽입, key 있으면 데이터수정
+    pop         데이터삭제
+'''
+
 members ={'홍길동':'010-1111-2222','박길동':'010-3333-4444'}
 print(members)
 
 members.setdefault('윤길동','010-5555-6666')
 members.setdefault('이길동')
 print(members)
+print()
 
-members.update(이길동='010-7777-8888')
+members.update(이길동='010-7777-8888')     # 새데이터 삽입O, 기존데이터 수정X
 print(members)
 members.update(한길동='010-9999-0000')
 print(members)
+members.update({2023082501:'010-2345-6789'})
+members.update([[2023082502, '010-1234-5678'], ['고길동', '010-4566-2345']])
+members.update(zip(['park', '이길동'],['8282', None]))
+print(members)
+print()
 
-
+value = members.pop('이길동')
+print(f'pop("이길동") : {value}')
 members.pop('구길동', "없음")
 print(members)
-
 members.popitem()
 print(members)
+print()
 
 value = members.get('홍길동')
 print(f'홍길동 : {value}')
